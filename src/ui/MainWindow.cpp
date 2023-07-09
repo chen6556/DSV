e0d72f1c5cb0867ee1ef3620cf5c868adfe4ec34
ChenL <liuyuchen6556@163.com>
2023-07-09 11:10:32 +0800

    增加PDFSpirit。

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -1,11 +1,14 @@
 #include "ui/MainWindow.hpp"
 #include "./ui_mainwindow.h"
 #include "io/File.hpp"
+#include "io/PDFSpirit.hpp"
 #include <QFileDialog>
 #include <QMessageBox>
 #include <QJsonObject>
 #include <QJsonDocument>
 #include <QAbstractItemView>
+#include <qpdf/QPDF.hh>
+#include <qpdf/QPDFWriter.hh>
  
 
 MainWindow::MainWindow(QWidget *parent)
@@ -94,12 +97,35 @@ void MainWindow::open_file()
     QFileDialog *dialog = new QFileDialog();
     dialog->setModal(true);
     dialog->setFileMode(QFileDialog::ExistingFile);
-    QString path = dialog->getOpenFileName(dialog, nullptr, _editer.path(), "Files: (*.json *.JSON)");
+    QString path = dialog->getOpenFileName(dialog, nullptr, _editer.path(), "Files: (*.json *.JSON);;PDF: (*.pdf *.PDF)");
     if (!path.isEmpty())
     {
         _editer.delete_graph();
         Graph *g = new Graph;
-        File::read(path, g);
+
+        if (path.endsWith(".json") || path.endsWith(".JSON"))
+        {
+            File::read(path, g);
+        }
+        else if (path.endsWith(".pdf") || path.endsWith(".PDF"))
+        {
+            QPDF pdf;
+            pdf.processFile(path.toStdString().c_str());
+
+            QPDFWriter outpdf(pdf);
+            outpdf.setStreamDataMode(qpdf_stream_data_e::qpdf_s_uncompress);
+            outpdf.setDecodeLevel(qpdf_stream_decode_level_e::qpdf_dl_all);
+            outpdf.setOutputMemory();
+            outpdf.setNewlineBeforeEndstream(true);
+            outpdf.write();
+            std::shared_ptr<Buffer> buffer = outpdf.getBufferSharedPointer();
+
+            std::stringstream ss(reinterpret_cast<char *>(buffer->getBuffer()), std::ios::in);
+            PDFSpirit spirit;
+            spirit.load_graph(g);
+            spirit.parse(ss);
+        }
+
         _editer.load_graph(g, path);
         _editer.reset_modified();
         _info_labels[2]->setText(path);
