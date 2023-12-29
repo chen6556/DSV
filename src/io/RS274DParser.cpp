f8e97a58b5b5a524d0dddc20b4524ca4df3f3985
chen6556 <liuyuchen6556@163.com>
2023-12-29 09:31:00 +0800

    增加Table指令。

--- src/io/RS274DParser.cpp
+++ src/io/RS274DParser.cpp
@@ -149,19 +149,19 @@ Importer importer;
 Parser<bool> blank = ch_p(' ') | ch_p('\t') | ch_p('\v');
 Parser<char> separator = ch_p('*');
 
-Parser<bool> skip_cmd = (ch_p('H') >> int_p()) >> separator;
+Parser<bool> skip_cmd = ((ch_p('H') >> int_p()) | ch_p('Q')) >> !separator;
 
 // 坐标设置
 Action<int> x_coord_a(&importer, &Importer::set_x_coord);
 Action<int> y_coord_a(&importer, &Importer::set_y_coord);
 Parser<bool> coord = ch_p('X') >> int_p()[x_coord_a] >>
-                    ch_p('Y') >> int_p()[y_coord_a] >> separator;
+                    ch_p('Y') >> int_p()[y_coord_a] >> !separator;
 // 单位设置 mil实际为100mil，换算为2.54mm
 Action<void> set_mm_unit_a(&importer, &Importer::set_unit_mm);
 Action<void> set_mil_unit_a(&importer, &Importer::set_unit_hectomil);
 
 Parser<std::string> set_mm_unit = str_p("G71")[set_mm_unit_a];
-Parser<std::string> set_mil_unit = str_p("G72")[set_mil_unit_a];
+Parser<bool> set_mil_unit = (str_p("G72") | str_p("G70"))[set_mil_unit_a];
 
 Parser<bool> set_unit = (set_mm_unit | set_mil_unit) >> separator;
 
@@ -199,7 +199,8 @@ Parser<bool> circle = (circle10 | circle20 | circle30 | circle40) >> separator;
 
 // 文字处理
 Action<std::string> a_text(&importer, &Importer::store_text);
-Parser<bool> text = confix_p(str_p("M31") >> separator, (*anychar_p())[a_text], separator);
+Parser<bool> text = confix_p(str_p("M31*"), (*anychar_p())[a_text], separator);
+Parser<bool> skip_text = confix_p(str_p("M20*"), *anychar_p(), separator);
 
 // 步骤
 Parser<bool> steps = ch_p('N') >> int_p() >> separator;
@@ -207,12 +208,22 @@ Parser<bool> steps = ch_p('N') >> int_p() >> separator;
 Parser<bool> end = str_p("M0") >> separator;
 // 未知命令
 Action<std::string> a_unkown(&importer, &Importer::print_symbol);
-Parser<bool> unkown_cmds = confix_p(alphaa_p(), (*anychar_p())[a_unkown], separator);
 
-Parser<bool> cmd = *(eol_p() | coord | set_unit | pen_move | interp | circle | steps | text | blank | skip_cmd | separator);
-Parser<bool> terminal_cmd = end | unkown_cmds;
-Parser<bool> rs274 = cmd >> !terminal_cmd;
-// Parser<bool> cmd = *(coord | set_unit | pen_size | pen_move | interp | circle | steps | blank | eol_p() | unkown_cmds);
+Parser<bool> unkown_cmds = confix_p(alphaa_p(), (*anychar_p())[a_unkown], separator) - end;
+
+Parser<bool> cmd = *(eol_p() | coord | set_unit | pen_move | interp | circle | steps | text | skip_text | blank | skip_cmd | separator | unkown_cmds);
+
+Parser<bool> table_start = str_p("N,0001") >> eol_p();
+Parser<bool> rest_of_line = *(anychar_p() - eol_p());
+Parser<bool> unknown_gap_line = rest_of_line >> eol_p();
+Parser<bool> unknown_gap = *(unknown_gap_line - table_start);
+Parser<bool> table_line = rest_of_line >> eol_p();
+Parser<bool> position_line = str_p("P,") >> int_p() >> ch_p(',') >> int_p() >> rest_of_line >> eol_p();
+Parser<bool> text_line = str_p("D,") >> int_p() >> ch_p(',') >> rest_of_line >> eol_p();
+Parser<bool> table_end = str_p("L0*") >> !eol_p();
+Parser<bool> table = confix_p(table_start, *(text_line | position_line | table_line), table_end);
+
+Parser<bool> rs274 = cmd >> !end >> *(anychar_p() - table_start) >> !table;
 
 
 bool parse(std::string_view &stream, Graph *graph)
