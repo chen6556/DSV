6d494fd0876c5f3e23e992f03c96df7d70a81c64
chen6556 <liuyuchen6556@163.com>
2024-01-26 14:12:18 +0800

    Editer加载和删除Graph时重置_current_group。

--- src/base/Editer.cpp
+++ src/base/Editer.cpp
@@ -67,6 +67,7 @@ void Editer::load_graph(Graph *graph, const QString &path)
     if (graph != nullptr)
     {
         _graph = graph;
+        _current_group = 0;
         init();
         _file_path = path;   
     }
@@ -77,6 +78,7 @@ void Editer::load_graph(Graph *graph)
     if (graph != nullptr)
     {
         _graph = graph;
+        _current_group = 0;
         init();
         _file_path.clear();  
     }
@@ -88,12 +90,13 @@ void Editer::delete_graph()
     {
         delete _graph;
         _graph = nullptr;
+        _current_group = 0;
         _file_path.clear();
         while (!_backup.empty())
         {
             delete _backup.back();
             _backup.pop_back();
-        }   
+        }
     }
 }
 
