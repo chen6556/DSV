ef6fa604611d20fa0290828ecafa157e2476b7b1
chen6556 <liuyuchen6556@163.com>
2024-05-17 16:29:45 +0800

    添加启动动画.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "ui/MainWindow.hpp"
 
-#include <QStyleFactory>
+#include <QSplashScreen>
 #include <QApplication>
 
 
@@ -9,9 +9,13 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
     QApplication a(argc, argv);
 
+    QSplashScreen splash(QPixmap(":/icons/DSV_logo.png"));
+    splash.show();
+
     MainWindow w;
 
     w.show();
+    splash.finish(&w);
 
     return a.exec();
 }
